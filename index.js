548f7c9d1446130f5eab21632f29a1df746feff9
Filip Tammergård <filip@tammergard.se>
2021-09-22 12:27:22 +0200

    feat: set all options explicitly
    To avoid unwanted changed when defaults are changed.

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-// defaults are commented out
+// defaults in comments
 
 module.exports = {
-  // printWidth: 80,
-  // tabWidth: 2,
-  // useTabs: false,
-  semi: false,
-  // singleQuote: false,
-  // quoteProps: "as-needed",
-  // jsxSingleQuote: false,
-  trailingComma: "all",
-  // bracketSpacing: true,
-  // jsxBracketSameLine: false,
-  // arrowParens: "always",
-  // rangeStart: 0,
-  // rangeEnd: Infinity,
-  // requirePragma: false,
-  // insertPragma: false,
-  proseWrap: "always",
-  // htmlWhitespaceSensitivity: "css",
-  // endOfLine: "lf",
-  // embeddedLanguageFormatting: "auto",
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false, // true
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all", // "es5"
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "always", // "preserve"
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
   overrides: [
+    // allow longer line lengths in JSON files
     {
       files: "*.json",
       options: {
